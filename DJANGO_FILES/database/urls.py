85df7a88108a2fdcc9f6f6510d22b6ab21598f86
ShaluSenthilnathan <1RN20CS137@RNSIT.onmicrosoft.com>
2023-02-06 10:07:26 +0530

    uploading initial code

---
+++ DJANGO_FILES/database/urls.py
@@ -0,0 +1,43 @@
+"""database URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path,include 
+from django.urls import re_path as url
+
+from dataentry.api import StudentList,AadharList
+from dataentry.api import AttendanceList,MarksList
+from dataentry.api import FeesList,HostelList
+from dataentry.api import DepartmentList
+from dataentry.api import ImpInfoList,TeacherList
+from dataentry.api import BacklogsList
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+  
+    url(r'api/students_list/$',StudentList.as_view(),name='student_list'),
+    url(r'api/attendance_list/$',AttendanceList.as_view(),name='attendance_list'),
+    url(r'api/marks_list/$',MarksList.as_view(),name='marks_list'),
+    url(r'api/aadhar_list/$',AadharList.as_view(),name='aadhar_list'),
+    url(r'api/fees_list/$',FeesList.as_view(),name='fees_list'),
+    url(r'api/teachers_list/$',TeacherList.as_view(),name='teacher_list'),
+    url(r'api/hostel_list/$',HostelList.as_view(),name='hostel_list'),
+    url(r'api/imfinfo_list/$',ImpInfoList.as_view(),name='impinfo_list'),
+    url(r'api/departmentinfo_list/$',DepartmentList.as_view(),name='department_list'),
+    url(r'api/departmentcourses_list/$',DepartmentList.as_view(),name='departmentcourse_info'),
+    url(r'api/backlogs_list/$',BacklogsList.as_view(),name='backlogs_list'),  
+    
+    path('',include('main.urls')),
+]
---
+++ DJANGO_FILES/dataentry/api.py
@@ -0,0 +1,157 @@
+from rest_framework.views import APIView 
+from rest_framework.response import Response 
+from rest_framework import status 
+from . serializers import *
+
+class StudentList(APIView):
+    def get(self,request):
+        model = Student.objects.all()
+        serializer = StudentSerializer(model,many=True)
+        return Response(serializer.data)
+    
+    def post(self,request):
+        serializer = StudentSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status=status.HTTP_201_CREATED)
+        return Response(serializer.errors,status=status.HTTP_404_NOT_FOUND)
+        
+            
+        
+class AttendanceList(APIView):
+    def get(self,request):
+        model = Attendance.objects.all()
+        serializer = AttendanceSerializer(model,many=True)
+        return Response(serializer.data)
+    
+    def post(self,request):
+        serializer = AttendanceSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status=status.HTTP_201_CREATED)
+        return Response(serializer.errors,status=status.HTTP_404_NOT_FOUND)
+    
+    
+class MarksList(APIView):
+    def get(self,request):
+        model = Marks.objects.all()
+        serializer = MarksSerializer(model,many=True)
+        return Response(serializer.data)
+    
+    def post(self,request):
+        serializer = MarksSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status=status.HTTP_201_CREATED)
+        return Response(serializer.errors,status=status.HTTP_404_NOT_FOUND)
+    
+    
+class AadharList(APIView):
+    def get(self,request):
+        model = Aadhar.objects.all()
+        serializer = AadharSerializer(model,many=True)
+        return Response(serializer.data)
+    
+    def post(self,request):
+        serializer = AadharSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status=status.HTTP_201_CREATED)
+        return Response(serializer.errors,status=status.HTTP_404_NOT_FOUND)
+    
+class FeesList(APIView):
+    def get(self,request):
+        model = Fees.objects.all()
+        serializer = FeesSerializer(model,many=True)
+        return Response(serializer.data)
+    
+    def post(self,request):
+        model = Fees.objects.all()
+        serializer = FeesSerializer(model,data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status=status.HTTP_201_CREATED)
+        return Response(serializer.errors,status=status.HTTP_404_NOT_FOUND)
+    
+    
+class DepartmentList(APIView):
+    def get(self,request):
+        model = Departmentlist.objects.all()
+        serializer = DepartmentListSerializer(model,many=True)
+        return Response(serializer.data)
+    
+    def post(self,request):
+        serializer = DepartmentListSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status=status.HTTP_201_CREATED)
+        return Response(serializer.errors,status=status.HTTP_404_NOT_FOUND)
+    
+class DepartmentCourseInfo(APIView):
+    def get(self,request):
+        model = Departmentcourseinfo.objects.all()
+        serializer = DepartmentCourseInfoSerializer(model,many=True)
+        return Response(serializer.data)
+    
+    def post(self,request):
+        serializer = DepartmentCourseInfoSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status=status.HTTP_201_CREATED)
+        return Response(serializer.errors,status=status.HTTP_404_NOT_FOUND)
+    
+    
+    
+class HostelList(APIView):
+    def get(self,request):
+        model = Hostel.objects.all()
+        serializer = HostelSerializer(model,many=True)
+        return Response(serializer.data)
+    
+    def post(self,request):
+        serializer = HostelSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status=status.HTTP_201_CREATED)
+        return Response(serializer.errors,status=status.HTTP_404_NOT_FOUND)
+    
+    
+class ImpInfoList(APIView):
+    def get(self,request):
+        model = ImpInfo.objects.all()
+        serializer = ImpInfoSerializer(model,many=True)
+        return Response(serializer.data)
+    
+    def post(self,request):
+        serializer = ImpInfoSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status=status.HTTP_201_CREATED)
+        return Response(serializer.errors,status=status.HTTP_404_NOT_FOUND)
+    
+    
+class TeacherList(APIView):
+    def get(self,request):
+        model = Teacherslist.objects.all()
+        serializer = TeacherSerializer(model,many=True)
+        return Response(serializer.data)
+    
+    def post(self,request):
+        serializer = TeacherSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status=status.HTTP_201_CREATED)
+        return Response(serializer.errors,status=status.HTTP_404_NOT_FOUND)
+    
+class BacklogsList(APIView):
+    def get(self,request):
+        model = Backlogs.objects.all()
+        serializer = BacklogsSerializer(model,many=True)
+        return Response(serializer.data)
+    
+    def post(self,request):
+        serializer = BacklogsSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status=status.HTTP_201_CREATED)
+        return Response(serializer.errors,status=status.HTTP_404_NOT_FOUND)
